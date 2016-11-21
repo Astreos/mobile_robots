f8ab865225b2e46b0a869e1b50b9ead67d6785fb
Nicolas <spideraider@gmail.com>
2016-11-21 18:12:15 +0100

    Ajout d'une fonction run pour prendre les deux coordonnées

--- userFiles/ctrl/groups_ctrl/gr18/regulation/path_regulation_gr18.cc
+++ userFiles/ctrl/groups_ctrl/gr18/regulation/path_regulation_gr18.cc
@@ -160,4 +160,48 @@ int run_y(CtrlStruct *cvs, double y_ref)
     pos_reg->last_t = inputs->t;
 }
 
+int run(CtrlStruct *cvs, double x_ref, double y_ref)
+{
+    double dt;
+
+    // variables declaration
+    RobotPosition *rob_pos;
+    CtrlIn *inputs;
+    CtrlOut *outputs;
+    PosRegulation *pos_reg;
+
+    // variables initialization
+    inputs  = cvs->inputs;
+    outputs = cvs->outputs;
+    rob_pos = cvs->rob_pos;
+    pos_reg  = cvs->pos_reg;
+
+    // time
+    dt = inputs->t - pos_reg->last_t; // time interval since last call
+
+    // ----- Wheels regulation computation start ----- //
+
+    int Kp = 50;
+    float Ti = 100;
+
+    pos_reg->int_error_r = nom_dist(x_ref - rob_pos->x, y_ref - rob_pos->y)*dt + pos_reg->int_error_r;
+    pos_reg->int_error_l = nom_dist(x_ref - rob_pos->x, y_ref - rob_pos->y)*dt + pos_reg->int_error_l;
+
+    speed_regulation(cvs, -Kp*nom_dist(x_ref - rob_pos->x, y_ref - rob_pos->y) - (Kp/Ti)*limit_range(pos_reg->int_error_r, -3.0, 3.0), -Kp*nom_dist(x_ref - rob_pos->x, y_ref - rob_pos->y) - (Kp/Ti)*limit_range(pos_reg->int_error_l, -3.0, 3.0));
+
+    // ----- Wheels regulation computation end ----- //
+
+    if (fabs(nom_dist(x_ref - rob_pos->x, y_ref - rob_pos->y)) < 0.0001)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+
+    // last update time
+    pos_reg->last_t = inputs->t;
+}
+
 NAMESPACE_CLOSE();
