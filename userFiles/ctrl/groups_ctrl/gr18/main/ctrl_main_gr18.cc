b914b25be96b7b501c45fa7406de5242912d94b2
Nicolas <spideraider@gmail.com>
2017-02-03 10:45:52 +0100

    Dummy commit

--- userFiles/ctrl/groups_ctrl/gr18/main/ctrl_main_gr18.cc
+++ userFiles/ctrl/groups_ctrl/gr18/main/ctrl_main_gr18.cc
@@ -1,4 +1,4 @@
-/*! 
+/*!
  * \author Group 18
  * \file controller_main_gr18.cc
  * \brief Initialization, loop and finilization of the controller written in C (but compiled as C++)
@@ -21,19 +21,19 @@
 NAMESPACE_INIT(ctrlGr18);
 
 /*! \brief initialize controller operations (called once)
- * 
+ *
  * \param[in] cvs controller main structure
  */
 void controller_init(CtrlStruct *cvs)
 {
 	// variables declaration
 	CtrlIn *inputs;
-	
+
 	double t;
-	
+
 	// variables initialization
 	inputs = cvs->inputs;
-	
+
 	t = inputs->t;
 
 	// robot ID
@@ -42,16 +42,16 @@ void controller_init(CtrlStruct *cvs)
 	// robot team
 	switch (inputs->robot_id)
 	{
-		case ROBOT_B: cvs->team_id = TEAM_A; break;
-		case ROBOT_R: cvs->team_id = TEAM_A; break;
-		case ROBOT_Y: cvs->team_id = TEAM_B; break;
-		case ROBOT_W: cvs->team_id = TEAM_B; break;
-		
-		default:
-			printf("Error: unknown robot ID: %d !\n", inputs->robot_id);
-			exit(EXIT_FAILURE);
+	case ROBOT_B: cvs->team_id = TEAM_A; break;
+	case ROBOT_R: cvs->team_id = TEAM_A; break;
+	case ROBOT_Y: cvs->team_id = TEAM_B; break;
+	case ROBOT_W: cvs->team_id = TEAM_B; break;
+
+	default:
+		printf("Error: unknown robot ID: %d !\n", inputs->robot_id);
+		exit(EXIT_FAILURE);
 	}
-	
+
 	// number of opponents
 	cvs->nb_opp = inputs->nb_opponents;
 
@@ -63,17 +63,17 @@ void controller_init(CtrlStruct *cvs)
 	cvs->sp_reg->last_t = t;
 	cvs->sp_reg->int_error_r = 0;
 	cvs->sp_reg->int_error_l = 0;
-        
+
 	// position regulation
 	cvs->pos_reg->last_t = t;
 	cvs->pos_reg->int_error_r = 0;
 	cvs->pos_reg->int_error_l = 0;
-	
+
 	return;
 }
 
 /*! \brief controller loop (called every time-step)
- * 
+ *
  * \param[in] cvs controller main structure
  */
 void controller_loop(CtrlStruct *cvs)
@@ -81,9 +81,9 @@ void controller_loop(CtrlStruct *cvs)
 	// variables declaration
 	CtrlIn *inputs;
 	CtrlOut *outputs;
-	
+
 	double t;
-	
+
 	// variables initialization
 	inputs  = cvs->inputs;
 	outputs = cvs->outputs;
@@ -105,64 +105,64 @@ void controller_loop(CtrlStruct *cvs)
 
 	switch (cvs->main_state)
 	{
-		// calibration
-		case CALIB_STATE:
-			calibration(cvs);
-			break;
+	// calibration
+	case CALIB_STATE:
+		calibration(cvs);
+		break;
 
-		// wait before match beginning
-		case WAIT_INIT_STATE:
-			speed_regulation(cvs, 0.0, 0.0);
-			
-			// kalman
-			cvs->kalman_pos->x = cvs->rob_pos->x;
-			cvs->kalman_pos->y = cvs->rob_pos->y;
-			cvs->kalman_pos->theta = cvs->rob_pos->theta;
+	// wait before match beginning
+	case WAIT_INIT_STATE:
+		speed_regulation(cvs, 0.0, 0.0);
 
-			if (t > 0.0)
-			{
-				cvs->main_state = RUN_STATE;
-				cvs->strat->main_state = FIRST_TARGET;
-				cvs->strat->sub_state = TRAJECTORY;
-				cvs->pos_reg->path_state = FOLLOW_CHECKPOINTS;
-			}
-			break;
+		// kalman
+		cvs->kalman_pos->x = cvs->rob_pos->x;
+		cvs->kalman_pos->y = cvs->rob_pos->y;
+		cvs->kalman_pos->theta = cvs->rob_pos->theta;
 
-		// during game
-		case RUN_STATE:
-			// kalman
-			kalman(cvs);
-			
-			main_strategy(cvs);
+		if (t > 0.0)
+		{
+			cvs->main_state = RUN_STATE;
+			cvs->strat->main_state = FIRST_TARGET;
+			cvs->strat->sub_state = TRAJECTORY;
+			cvs->pos_reg->path_state = FOLLOW_CHECKPOINTS;
+		}
+		break;
 
-			if (t > 89.0) // 1 second safety
-			{
-				cvs->main_state = STOP_END_STATE;
-			}
-			break;
+	// during game
+	case RUN_STATE:
+		// kalman
+		kalman(cvs);
 
-		// stop at the end of the game
-		case STOP_END_STATE:
-			speed_regulation(cvs, 0.0, 0.0);
+		main_strategy(cvs);
 
-			outputs->flag_release = 1;
-			break;
+		if (t > 89.0) // 1 second safety
+		{
+			cvs->main_state = STOP_END_STATE;
+		}
+		break;
 
-		case NB_MAIN_STATES:
-			printf("Error: state NB_MAIN_STATES should not be reached !\n");
-			exit(EXIT_FAILURE);
-			break;
+	// stop at the end of the game
+	case STOP_END_STATE:
+		speed_regulation(cvs, 0.0, 0.0);
 
-		default:
-			printf("Error:unknown state : %d !\n", cvs->main_state);
-			exit(EXIT_FAILURE);
+		outputs->flag_release = 1;
+		break;
+
+	case NB_MAIN_STATES:
+		printf("Error: state NB_MAIN_STATES should not be reached !\n");
+		exit(EXIT_FAILURE);
+		break;
+
+	default:
+		printf("Error:unknown state : %d !\n", cvs->main_state);
+		exit(EXIT_FAILURE);
 	}
-	
+
 	return;
 }
 
 /*! \brief last controller operations (called once)
- * 
+ *
  * \param[in] cvs controller main structure
  */
 void controller_finish(CtrlStruct *cvs)
